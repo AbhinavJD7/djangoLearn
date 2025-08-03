5f74a706799c2937b3179d968ac91bc6674eef53
Abhinav Rai <122885696+AbhinavJD7@users.noreply.github.com>
2025-08-03 14:54:46 +0530

    Added hello app

--- djangoBasics/djangoBasics/views.py
+++ djangoBasics/djangoBasics/views.py
@@ -6,7 +6,9 @@ def home(request):
     return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse("Hello World you are at about page")
+    #return HttpResponse("Hello World you are at about page")
+    return render(request, 'about.html')
 
 def contact(request):
-    return HttpResponse("Hello World you are at contact page")
+    #return HttpResponse("Hello World you are at contact page")
+    return render(request , 'contact.html')
